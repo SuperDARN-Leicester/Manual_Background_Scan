194f0a3940d919bfef45232009729b13be6d2de2
Matthew Jones <mj299@leicester.ac.uk>
2025-07-02 09:40:16 +0000

    Tidied Man_Bkgrd_Scan_FFT and Man_Bkgrd_Scan
    Man_Bkgrd_Scan_FFT and Man_Bkgrd_Scan updated to have better commenting and minor changes.
    gitignore files added to subfolders to ensure correct folders are present for files to be moved into using scripts.

--- Man_Bkgrd_Scan.py
+++ Man_Bkgrd_Scan.py
@@ -1,9 +1,19 @@
+"""
+Man_Bkgrd_Scan
+~~~~~~~~~~~~~~~~~~
+This script uses the USRP 'rx_samples_to_file' utility to listen on all 20 off receive channels (16 off main, 4 off interferometer) accross all 10 licenced bands.
+It produces 200 binary files which are saved to the /home/radar/UOL_scripts/Manual_Background_Scan/dat_files/ directory.
+NOTE: The 'rx_samples_to_file' calls are made in series and are fairly time consuming. So this script takes a while to run.
+
+:copyright: 2020-2025 University of Leicester
+"""
+
 import subprocess
 
 bands = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-freqs = [9090e3, 9947.5e3, 11125e3, 11575e3, 13475e3, 13885e3, 16295e3, 18041e3, 19547.5e3, 19895e3]
-rate = 301205
-duration = 0.1
+freqs = [9090e3, 9947.5e3, 11125e3, 11575e3, 13475e3, 13885e3, 16295e3, 18041e3, 19547.5e3, 19895e3] # Centre frequency of licenced bands, Hz
+rate = 301205 # sample rate (samples per second)
+duration = 1 # RX duration, s (beware - this is done 200 times + some overhead for each 'rx_samples_to_file' call!)
 
 # Generate list of N200 IP addresses
 addr = []
@@ -18,24 +28,27 @@ for x in range (0,10):
     for y in range (0, 4):
         filenames.append("Clear_Scan_Intf{}_Band{}".format(y, x))
 
-# Run RX scans on main array
-filename_index = 0
-for x in range (0,10):
+filename_index = 0 # Index used to to extract filename from list for each 'rx_samples_to_file' call
+for x in range (0,10): # loop through each TX band
     print("================= Band {} ==========================".format(x))
+
+    # Run scans on 16 off main array RX channels
     for y in range (0, 16):
         cmd = "rx_samples_to_file --args addr={} --subdev A:A --ref external --freq {} --rate {} --duration {} --file {}.dat".format(addr[y], freqs[x], rate, duration, filenames[filename_index])
         print(cmd)
-        returned_value = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)  # returns the exit code in unix
-        print('returned value:', returned_value)
+        returned_value = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) # Run 'rx_samples_to_file' command and surpress output
+        print('returned value:', returned_value) # returns the exit code in unix
         filename_index += 1
-    # Run RX scans on interferometer array
+
+    # Run scans on 4 off interferometer RX channels
     for y in range (0, 4):
         cmd = "rx_samples_to_file --args addr={} --subdev A:B --ref external --freq {} --rate {} --duration {} --file {}.dat".format(addr[y+4], freqs[x], rate, duration, filenames[filename_index])
         print(cmd)
-        returned_value = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)  # returns the exit code in unix
-        print('returned value:', returned_value)
+        returned_value = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)  # Run 'rx_samples_to_file' command and surpress output
+        print('returned value:', returned_value) # returns the exit code in unix
         filename_index += 1
-        # Move files into UoL script path
+
+    # Move files into UoL script path
     cmd = """mv -- "Clear_Scan"* /home/radar/UOL_scripts/Manual_Background_Scan/dat_files/"""
     print(cmd)
     returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
--- Man_Bkgrd_Scan_FFT.py
+++ Man_Bkgrd_Scan_FFT.py
@@ -1,13 +1,22 @@
+"""
+Man_Bkgrd_Scan_FFT
+~~~~~~~~~~~~~~~~~~
+This script takes binary files produced using USRP 'rx_samples_to_file' utility using the 'Man_Bkgrd_Scan.py' script, performs an FFT and saves an image of each spectrum.
+Spectrums are produced for all 20 off receive channels (16 off main, 4 off interferometer) accross all 10 licenced bands.
+It produces 200 FFT image files (png) which are saved to the /home/radar/UOL_scripts/Manual_Background_Scan/exported_images/BandX/ directory.
+NOTE: The centre frequencies and sample rate *must* match those used to generate the binary files or the FFTs produced will be wrong!
+
+:copyright: 2020-2025 University of Leicester
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
 # Declare constants
 bands = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-centre_f = [9090e3, 9947.5e3, 11125e3, 11575e3, 13475e3, 13885e3, 16295e3, 18041e3, 19547.5e3, 19895e3]
-low_freq = [9040e3, 9900e3, 11075e3, 11550e3, 13440e3, 13870e3, 16210e3, 18030e3, 19415e3, 19800e3]
-high_freq = [9140e3, 9995e3, 11175e3, 11600e3, 13510e3, 13900e3, 16380e3, 18052e3, 19680e3, 19990e3]
+centre_f = [9090e3, 9947.5e3, 11125e3, 11575e3, 13475e3, 13885e3, 16295e3, 18041e3, 19547.5e3, 19895e3] # Centre frequency of licenced bands, Hz
+low_freq = [9040e3, 9900e3, 11075e3, 11550e3, 13440e3, 13870e3, 16210e3, 18030e3, 19415e3, 19800e3] # Start frequency of licenced bands, Hz
+high_freq = [9140e3, 9995e3, 11175e3, 11600e3, 13510e3, 13900e3, 16380e3, 18052e3, 19680e3, 19990e3] # Stop frequency of licenced bands, Hz
 sample_rate = 301205 # sample rate (samples per second)
 IQ_format = 'short'
 sample_per = 1/sample_rate # Sample period (seconds)
@@ -24,14 +33,13 @@ for x in range (0,10):
         filenames.append("Clear_Scan_Intf{}_Band{}".format(y, x))
 
 # For each IQ file, generate fft and save image file
-filename_index = 0
-for x in range (0,10):
-    for y in range (0,20):
+filename_index = 0 # Index used to to extract filename from list for each 'rx_samples_to_file' call
+for x in range (0,10): # loop through each TX band
+    for y in range (0,20): # loop through all receive channels (16 off main, 4 off interferometer)
         # Read file
         IQ = np.fromfile('/home/radar/UOL_scripts/Manual_Background_Scan/dat_files/' + filenames[filename_index] + '.dat', dtype=np.int16)
 
-        # Trim file and convert to volts
-        IQ = IQ [500:]; # Trim off first 500 samples (settling time)
+        # Convert to volts
         LSB_volts = ADC_full_scale/ADC_levels;
         IQ = IQ*LSB_volts;
 
@@ -45,18 +53,20 @@ for x in range (0,10):
         timeax = np.linspace(0, ((L-1)*sample_per), L)
 
         # Plot IQ in time domain
-        fig, ax2 = plt.subplots()
-        # ax1.plot(timeax, I, label="I")
-        # ax1.plot(timeax, Q, label="Q")
-        # ax1.plot(timeax, np.sqrt(I**2 + Q**2), label="A")
-        # ax1.legend(loc="upper right")
-        # ax1.set_xlabel('Time (s)')
-        # ax1.set_ylabel('Voltage (V)')
+        #fig, ax1 = plt.subplots()
+        #ax1.plot(timeax, I, label="I")
+        #ax1.plot(timeax, Q, label="Q")
+        #ax1.plot(timeax, np.sqrt(I**2 + Q**2), label="A")
+        #ax1.legend(loc="upper right")
+        #ax1.set_xlabel('Time (s)')
+        #ax1.set_ylabel('Voltage (V)')
+        #plt.savefig('/home/radar/UOL_scripts/Manual_Background_Scan/exported_images/Band' + str(x) + '/' + filenames[filename_index] + '.png', bbox_inches='tight')
+        #plt.close
 
         # Perform complex FFT and normalise
         fftIQ = np.fft.fft(IQ)
         P = abs(fftIQ/L)
-        P = np.fft.fftshift(P) # Shifts zero to centre
+        P = np.fft.fftshift(P) # Shift the zero-freuqency component to the centre of the spectrum
 
         # Convert from V to dBm
         P = P * (1/(np.sqrt(2))) # From Vpeak to VRMS
@@ -67,9 +77,11 @@ for x in range (0,10):
         freqax = (freqax / 1e6) + (centre_f[x] / 1e6)
 
         # Plot FFT
+        fig, ax2 = plt.subplots()
         ax2.plot(freqax, P, linewidth=0.5)
         ax2.set_xlabel('Frequency (MHz)')
         ax2.set_ylabel('Power (dBm)')
+        plt.ylim(-130, 0)
         plt.title(filenames[filename_index])
         plt.axvspan(low_freq[x]*1e-6, high_freq[x]*1e-6, color='green', alpha=0.5)
         plt.savefig('/home/radar/UOL_scripts/Manual_Background_Scan/exported_images/Band' + str(x) + '/' + filenames[filename_index] + '.png', bbox_inches='tight')
